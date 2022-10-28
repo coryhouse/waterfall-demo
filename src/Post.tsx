7dd8d38dfe7aa93110acfce2a6975b194d37bdcd
Cory House <housecor@gmail.com>
2022-10-28 16:33:37 -0500

    Create detail page

---
+++ src/Post.tsx
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { getPost } from "./services/postService";
+
+export default function Post() {
+  const { postId } = useParams();
+  if (!postId) throw new Error("Invalid postId");
+
+  const postIdAsNumber = Number(postId);
+  const post = useQuery(["post", postIdAsNumber], () =>
+    getPost(postIdAsNumber)
+  );
+
+  if (post.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!post.data) {
+    return <div>Post not found</div>;
+  }
+
+  return (
+    <>
+      <h1>{post.data.title}</h1>
+    </>
+  );
+}
