0342e5853eb11abfd903a3c1b3f3e7a3e19e7856
Cory House <housecor@gmail.com>
2022-10-28 17:34:14 -0500

    Add loading state

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,15 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
+import { getLinks } from "./services/linkService";
 
 export default function Nav() {
+  const links = useQuery(["links"], getLinks, { initialData: [] });
+
+  if (links.isLoading) {
+    return <div>Loading links...</div>;
+  }
+
   return (
     <nav>
       <ul>
-        <li>
-          <Link to="/">Posts</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
+        {links.data.map((link) => (
+          <li key={link.id}>
+            <Link to={link.url}>{link.text}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
