e28dbeda3d56961d5da5ffdff019f0318a76eccd
Cory House <housecor@gmail.com>
2022-10-28 17:29:07 -0500

    Add nav

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Posts from "./Posts";
 import Post from "./Post";
+import Nav from "./Nav";
+import About from "./About";
 
 const queryClient = new QueryClient();
 
@@ -15,9 +17,11 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <Nav />
         <Routes>
           <Route path="/" element={<Posts />} />
           <Route path="/post/:postId" element={<Post />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
