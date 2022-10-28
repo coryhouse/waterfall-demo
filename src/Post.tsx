702326fede6fd7cc6f1039a980b6df475efde6ec
Cory House <housecor@gmail.com>
2022-10-28 17:11:45 -0500

    Show comments

--- src/Post.tsx
+++ src/Post.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import { getComments } from "./services/commentService";
 import { getPost } from "./services/postService";
 
 export default function Post() {
@@ -7,11 +8,16 @@ export default function Post() {
   if (!postId) throw new Error("Invalid postId");
 
   const postIdAsNumber = Number(postId);
+
   const post = useQuery(["post", postIdAsNumber], () =>
     getPost(postIdAsNumber)
   );
 
-  if (post.isLoading) {
+  const comment = useQuery(["comment", postIdAsNumber], () =>
+    getComments(postIdAsNumber)
+  );
+
+  if (post.isLoading || comment.isLoading) {
     return <div>Loading...</div>;
   }
 
@@ -22,6 +28,8 @@ export default function Post() {
   return (
     <>
       <h1>{post.data.title}</h1>
+      <h2>Comments</h2>
+      {comment.data && comment.data.map((comment) => <p>{comment.body}</p>)}
     </>
   );
 }
