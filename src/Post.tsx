ca454e4cdbadcfb2a8e1256057d3ca8e1c6ff04d
Cory House <housecor@gmail.com>
2022-10-28 20:06:53 -0500

    revert prefetch

--- src/Post.tsx
+++ src/Post.tsx
@@ -1,22 +1,15 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import Comments from "./Comments";
-import { getComments } from "./services/commentService";
 import { getPost } from "./services/postService";
 
 export default function Post() {
   const { postId: postIdString } = useParams();
-  const queryClient = useQueryClient();
   if (!postIdString) throw new Error("Invalid postId");
 
   const postId = Number(postIdString);
 
   const post = useQuery(["post", postId], () => getPost(postId));
-
-  // ⬇️ initiate a fetch before the comments component renders
-  queryClient.prefetchQuery(["comments", postId], () =>
-    getComments(Number(postId))
-  );
 
   if (post.isLoading) {
     return <div>Loading post...</div>;
