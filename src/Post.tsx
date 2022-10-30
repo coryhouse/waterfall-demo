9e368be81da0b063e948244b30b5e31ada30d1a8
Cory House <housecor@gmail.com>
2022-10-30 14:49:39 -0500

    Implement loader

--- src/Post.tsx
+++ src/Post.tsx
@@ -1,28 +1,43 @@
-import { useQuery } from "@tanstack/react-query";
+import { QueryClient, useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import Comments from "./Comments";
 import { getPost } from "./services/postService";
 
+const isNumeric = (num: any) =>
+  (typeof num === "number" || (typeof num === "string" && num.trim() !== "")) &&
+  !isNaN(num as number);
+
+const postQuery = (id: number) => ({
+  queryKey: ["post", id],
+  queryFn: () => getPost(id),
+});
+
+export const loader =
+  (queryClient: QueryClient) =>
+  async ({ params }: any) => {
+    const query = postQuery(params.id);
+    return (
+      queryClient.getQueryData(query.queryKey) ??
+      (await queryClient.fetchQuery(query))
+    );
+  };
+
 export default function Post() {
-  const { postId: postIdString } = useParams();
-  if (!postIdString) throw new Error("Invalid postId");
+  const { id } = useParams();
+  if (!id || Number.isInteger(id)) throw new Error("No id provided");
+  if (!isNumeric(id)) throw new Error("Invalid id provided");
+  const post = useQuery(postQuery(Number(id)));
 
-  const postId = Number(postIdString);
-
-  const post = useQuery(["post", postId], () => getPost(postId));
-
-  if (post.isLoading) {
-    return <div>Loading post...</div>;
-  }
-
-  if (!post.data) {
+  if (!post.isLoading && !post.data) {
     return <div>Post not found</div>;
   }
 
-  return (
+  return post.data ? (
     <>
       <h1>{post.data.title}</h1>
-      <Comments postId={postId} />
+      <Comments postId={post.data.id} />
     </>
+  ) : (
+    <>Loading post...</>
   );
 }
