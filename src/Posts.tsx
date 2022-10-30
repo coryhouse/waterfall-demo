6ef4d640e90506ff621514bffc11d3dccee3d892
Cory House <housecor@gmail.com>
2022-10-30 10:22:28 -0500

    Use Suspense, await, loader, no rq

--- src/Posts.tsx
+++ src/Posts.tsx
@@ -1,24 +1,37 @@
 import { useQuery } from "@tanstack/react-query";
-import { Link } from "react-router-dom";
+import { Suspense } from "react";
+import { Link, useLoaderData, defer, Await } from "react-router-dom";
 import { getPosts } from "./services/postService";
+import { Post } from "./types/Post.types";
+
+type LoaderResponse = {
+  posts: Promise<Post[]>;
+};
+
+export function loader() {
+  return defer({ posts: getPosts() });
+}
 
 export default function Posts() {
-  const posts = useQuery(["posts"], getPosts, { initialData: [] });
-
-  if (posts.isLoading) {
-    return <div>Loading...</div>;
-  }
+  const data = useLoaderData() as LoaderResponse;
 
   return (
     <>
       <h1>Posts</h1>
-      <ul>
-        {posts.data.map((post) => (
-          <li key={post.id}>
-            <Link to={"/post/" + post.id}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <Suspense fallback={<div>Loading posts...</div>}>
+        <Await resolve={data.posts} errorElement={<p>Error loading posts!</p>}>
+          {/* Can avoid the render prop below by extracting this to a separate component and using useLoaderData in that. https://reactrouter.com/en/main/guides/deferred */}
+          {(posts) => (
+            <ul>
+              {posts.map((post: Post) => (
+                <li key={post.id}>
+                  <Link to={"/post/" + post.id}>{post.title}</Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </Await>
+      </Suspense>
     </>
   );
 }
